70ac1a5e2235098d7b0cf1e620b360c11ecf1bc7
alnickelzon <al.villanueva1234@gmail.com>
2022-12-06 20:50:48 +0800

    update

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import styles from "./styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.rootContainer}>
+      <View style={styles.heading}>
+        <Text style={styles.title}>KOTLIN</Text>
+        <TextInput style={styles.input} placeholder="Search"></TextInput>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  heading: {
+    marginTop: 50,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+  },
+  input: {
+    borderRadius: 100 / 2,
+    backgroundColor: "#e8e8e8",
+    width: 150,
+    padding: 5,
+    textAlign: "center",
+  },
+});
